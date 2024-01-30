9d09d74669d897373d57e91d4f64b6909627a65b
nazmussaadatcse <nazmus.bd97@gmail.com>
2024-01-30 16:31:22 +0600

    loader fix

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,15 @@
-import Loading from "../Shared/Loading";
+import Image from 'next/image'
 
 
 const loading = () => {
     return (
-        <Loading></Loading>
+        <div className='w-full h-screen flex gap-4 justify-center items-center text-center absolute bg-white'>
+            <div className='m-2'>
+                <Image width="180" height="100" src={"https://i.ibb.co/9Tp3Wd4/FISLM.png"} alt="" />
+            </div>
+            <span className="loading text-blue-300 loading-bars loading-md"></span>
+            <span className="loading text-blue-400 loading-bars loading-lg"></span>
+        </div>
     );
 };
 
