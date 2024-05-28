07362c2f303a21bc7ba2bf1abc729823fc39c6ff
MD Nazmus Saadat <saadat@flyinfosoftbd.com>
2024-05-28 12:30:29 +0600

    kitchen profile bug fix

--- app/kitchen/profile/page.js
+++ app/kitchen/profile/page.js
@@ -4,10 +4,7 @@ import useUsers from '../../../Hooks/useUsers';
 import Kitchen from '../../kitchen/page'
 import { AuthContext } from '../../components/AuthProvider/AuthProvider';
 import useAxiosPublic from '../../../Hooks/useAxiosPublic';
-import Swal from 'sweetalert2'
-
 import Loading from '../../../Shared/Loading';
-import { useForm } from 'react-hook-form';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -18,12 +15,11 @@ const Page = () => {
     const { user } = useContext(AuthContext);
     const { users, refetch } = useUsers();
     const axiosPublic = useAxiosPublic();
-    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
 
     // const isAdmin = users.find(currentUser => currentUser.email === user?.email && currentUser.role === 'admin');
 
-    const currentUserData = users?.find(currentUser => currentUser.email === user?.email);
+    const currentUserData = users?.find(currentUser => currentUser?.email === user?.email);
     const isAdmin = currentUserData?.email === user?.email && currentUserData?.role === 'admin' ? true : false;
 
 
@@ -52,43 +48,43 @@ const Page = () => {
                             alt="Profile"
                             className="rounded-full mx-auto mb-4"
                         />
-                        <h2 className="text-2xl font-semibold text-gray-800 mb-4">{currentUserData?.orgName}</h2>
+                        <h2 className="text-2xl font-semibold text-gray-800 mb-4">{'currentUserData?.orgName'}</h2>
                         <p className="text-gray-600 text-xl">Kitchen Manager,</p>
-                        <p className="text-gray-600  mb-4">Type: {currentUserData?.orgType}</p>
+                        <p className="text-gray-600  mb-4">Type: {'currentUserData?.orgType'}</p>
                         <div className="flex items-center mt-4 ">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px] ">Name:</span>
-                            <span className="text-gray-800">{currentUserData?.managerName}</span>
+                            <span className="text-gray-800">{'currentUserData?.managerName'}</span>
                         </div>
 
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">Location:</span>
-                            <span className="text-gray-800">{currentUserData?.location}</span>
+                            <span className="text-gray-800">{'currentUserData?.location'}</span>
                         </div>
 
 
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">Phone:</span>
                             <Link href="tel:01303033418" className="text-blue-500 hover:underline">
-                            {currentUserData?.contactNumber}
+                            {'currentUserData?.contactNumber'}
                             </Link>
                         </div>
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">Email:</span>
                             <Link href="mailto:saadat.flyinfosoft@gmail.com" className="text-blue-500 hover:underline">
-                            {currentUserData?.email}
+                            {'currentUserData?.email'}
                             </Link>
                         </div>
 
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">UserID:</span>
-                            <span className="text-gray-800">{currentUserData?.username}</span>
+                            <span className="text-gray-800">{'currentUserData?.username'}</span>
                         </div>
 
 
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">Map:</span>
                             <Link
-                                href={currentUserData?.map}
+                                href={'currentUserData?.map'}
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="text-blue-500 hover:underline"
@@ -99,12 +95,12 @@ const Page = () => {
                         <div className="flex items-center mt-4">
                             <span className="text-gray-800 font-semibold mr-2 w-[66px]">Website:</span>
                             <Link
-                                href={currentUserData?.website}
+                                href={'currentUserData?.website'}
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="text-blue-500 hover:underline"
                             >
-                                {currentUserData?.website}
+                                {'currentUserData?.website'}
                             </Link>
                         </div>
                     </div>
