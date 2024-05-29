7463943bfc777fe5c788f9e432f9e757f02006db
MD Nazmus Saadat <saadat@flyinfosoftbd.com>
2024-05-29 12:32:15 +0600

    new server

--- Hooks/useAxiosPublic.jsx
+++ Hooks/useAxiosPublic.jsx
@@ -2,10 +2,11 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: 'https://fis-lunch-manager-server.vercel.app'
+    baseURL: 'https://fis-lunch-server.vercel.app'
 })
 // https://fis-lunch-manager-server.vercel.app 
 // http://localhost:5000
+// https://fis-lunch-server.vercel.app
 const useAxiosPublic = () => {
     
     return axiosPublic;
