e5377a68804536e8969d50526ec75c085890894d
MD Nazmus Saadat <saadat@flyinfosoftbd.com>
2024-08-25 11:26:30 +0600

    super

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -20,21 +20,24 @@ const Hero = () => {
     const [loading, setLoading] = useState(false);
     
     const [isAdmin, setIsAdmin] = useState(false);
+    const [isSuper, setIsSuper] = useState(false);
 
     useEffect(() => {
         // Check if the current user is an admin
         const adminUser = users.some(currentUser => currentUser.email === user?.email && currentUser.role === 'admin');
+        const isSuper = users.some(currentUser => currentUser.email === user?.email && currentUser?.super === 'yes');
         setIsAdmin(adminUser);
+        setIsSuper(isSuper);
         refetch()
         // console.log('clg')
     }, [users, user]);
 
-    // console.log(isAdmin)
+    // console.log( isSuper)
 
     const memoizedLunches = useMemo(() => {
         return lunches?.data?.map((lunch, index) => (
             <div key={index} className={`border p-2 text-sm rounded-md shadow ${lunch?.bookBy === 'admin' ? 'border border-white text-yellow-200' : ''}`}>
-                <div onClick={ isAdmin? () => handleUpdateMenuModal(lunch) : undefined   } >
+                <div onClick={ isSuper? () => handleUpdateMenuModal(lunch) : undefined   } >
                     {lunch?.name}
                     {lunch?.type === 'guest' && (
                         <span className='rounded-full border px-1 ml-1'>{lunch?.lunchQuantity}</span>
