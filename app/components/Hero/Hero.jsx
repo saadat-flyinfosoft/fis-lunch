9a29102dbc0e2d7d0a905732b5681e202e567218
MD Nazmus Saadat <saadat@flyinfosoftbd.com>
2025-08-04 10:27:03 +0600

    bug fix

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
     try {
         setNotifLoading(true);
         const res = await axiosPublic.post("/fire-notification", {
-        message: `${customMessage.trim()} — Sent by ${user.displayName}`,
+        message: `${customMessage.trim()}:${user.displayName}`,
         });
 
         if (res.data.message === 'Lunch notification sent successfully') {
